902aa8dbc9b562b134ce6b430d8115602065080e
Kaedyn Eastall (School) <eask23083@wainuiomatahigh.school.nz>
2025-03-13 09:56:46 +1300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 # Variable Setting
 INT_LIMIT = 100
 STR_SPEEDQUESTION = "How fast were you going (KM/H)? "
+STR_NAMEQUESTION = "What is your name? "
 int_userSpeed = 0
+str_userName = ''
 INT_FINE = 250
-STR_GOOD = "You are good to go!"
-STR_BAD = "You are being fined ${} for speeding.".format(INT_FINE)
+STR_GOOD = "Alright {}. You are good to go!"
+STR_BAD = "Alright {}. You are being fined ${} for speeding."
 
-# Start
+# Main
 print("Hello there, you have been pulled over for speeding.")
+str_userName = input(STR_NAMEQUESTION)
 str_userSpeed = int(input(STR_SPEEDQUESTION))
 
 if str_userSpeed < INT_LIMIT:
-    print(STR_GOOD)
+    print(STR_GOOD.format(str_userName))
 else:
-    print(STR_BAD)
+    print(STR_BAD.format(str_userName, INT_FINE))
