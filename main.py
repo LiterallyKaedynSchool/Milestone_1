68ec2233aa1578eac00a292e9f573ec255f3c2e8
Kaedyn Eastall (School) <eask23083@wainuiomatahigh.school.nz>
2025-03-14 14:34:33 +1300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 # Variable Setting
 INT_LIMIT = 101
 STR_SPEEDQUESTION = "How fast were you going (KM/H)? "
-STR_NAMEQUESTION = "What is your name? "
+STR_NAMEQUESTION = "What is your name? (Input \"END\" to end loop) "
 int_userSpeed = 0
-str_userName = ''
+str_userName = ""
 INT_FINE = 250
 STR_GOOD = "Alright {}. You are good to go!"
 STR_BAD = "Alright {}. You are being fined ${} for speeding."
+bool_again = True
 
 # Main
-print("Hello there, you have been pulled over for speeding.")
-str_userName = input(STR_NAMEQUESTION)
-str_userSpeed = int(input(STR_SPEEDQUESTION))
-
-if str_userSpeed < INT_LIMIT:
-    print(STR_GOOD.format(str_userName))
-else:
-    print(STR_BAD.format(str_userName, INT_FINE))
+while bool_again == True:
+    print("Hello there, you have been pulled over for speeding.")
+    str_userName = input(STR_NAMEQUESTION)
+    if str_userName == "END":
+        bool_again = False
+        continue
+    else:
+        int_userSpeed = int(input(STR_SPEEDQUESTION))
+    if int_userSpeed < INT_LIMIT:
+        print(STR_GOOD.format(str_userName))
+    else:
+        print(STR_BAD.format(str_userName, INT_FINE))
+    
+print("Bye!")
